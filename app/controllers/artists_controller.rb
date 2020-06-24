f42f6db50c9903969bde2768cae555416003bc04
DCR430 <daniel.c.reyes30@gmail.com>
2020-06-24 16:24:51 -0400

    Version 3

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,8 +11,22 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    
+    @artist = Artist.new
+
   
   end
+
+  def create 
+    
+    artist = Artist.create(artist_params)
+ end
+
+
+  private
+
+  def artist_params
+    params.require(artist).permit(:name,:age,:bio)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
  
  get '/artists', to: 'artists#index'
+ post '/artists', to: 'artists#create', as: 'create_artist'
  get '/artists/new', to: 'artists#new' , as: 'new_artist'
  get '/artists/:id', to: 'artists#show', as: 'artist'
+ 
 
 
 end
