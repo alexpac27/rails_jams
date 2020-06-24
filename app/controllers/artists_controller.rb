719974a037421464b22459b64355ab0ad47eea59
alexpac27 <alexpac27@gmail.com>
2020-06-24 16:50:06 -0400

    v4

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,20 +12,18 @@ class ArtistsController < ApplicationController
 
   def new
     @artist = Artist.new
-
-  
   end
 
   def create 
-    
     artist = Artist.create(artist_params)
+    redirect_to artist_path(artist.id)
  end
 
 
   private
 
   def artist_params
-    params.require(artist).permit(:name,:age,:bio)
+    params.require("artist").permit("name","age","bio")
   end
 
 
